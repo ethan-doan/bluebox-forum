99b8dcc0e77fca5ce26ba1c1ad39730a6a3178e6
ethandoan <ethan.doan@wilink.de>
2023-11-22 13:59:13 -0800

    Header Logo Colors Added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,14 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import "../styles/Header.css";
 
 function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand>
-          <span className="fw-bold">Blue</span>
-          <span>Box</span>
+          <span className="fw-bold logo-1">Blue</span>
+          <span className="logo-2">Box</span>
         </Navbar.Brand>
       </Container>
     </Navbar>
