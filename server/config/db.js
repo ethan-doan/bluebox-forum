c24284fd357d9b92d7eca214e0c1ae6604140528
ethandoan <ethan.doan@wilink.de>
2023-11-23 22:01:23 -0800

    Reinstalled Node and Fixed Errors

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,10 @@ const mongoDBURI = "mongodb://localhost:27017/bluebox-db";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoDBURI);
+    await mongoose.connect(mongoDBURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB...");
   } catch (err) {
     console.error("Could not connect to MongoDB:", err);
